d07aa725c3b11a4736264b273aa90576f4633f78
Ching-Hsiang (Tom) Hsu <chhsu@nyu.edu>
2018-03-10 18:37:18 -0500

    snake game

---
+++ Snake.cpp
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <math.h>
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <deque>
+#include <unordered_map>
+#include <random>
+
+class Snake {
+public:
+	Snake () {W=H=0;}
+	Snake (int w, int h):W(w), H(h) {}
+
+	void initial () {
+
+	}
+
+	void run () {
+
+	}
+
+	int report_length () {
+		return 0;
+	}
+
+	// randomly pick an index from 0~size-1
+	int get_random (int size) {
+        uniform_int_distribution<int> distribution(0, size-1);
+        return distribution(gen);
+    }
+
+	int W, H;
+	std::default_random_engine gen;
+	std::unordered_map<string, int> map_index;
+	std::unordered_map<int, string> map_location;
+};
+
+int main (int argc, char *argv[]) {
+
+	int w, h;
+	if (argc == 1) w = h = 10;
+	else if (argc == 2) w = h = atoi(argv[1]);
+	else {
+		w = atoi(argv[1]);
+		h = atoi(argv[2]);
+	}
+
+	Snake *snake = new Snake(w, h);
+	snake->initial ();
+	snake->run ();
+	std::cout << "Final Snake length: " << snake->report_length () << std::endl;
+
+	return 0;
+}
